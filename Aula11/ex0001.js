d1810f51fabaa9540035a2a3eaf1a8b7bf8e3860
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-05-07 09:34:41 -0300

    Aula 11

---
+++ Aula11/ex0001.js
@@ -0,0 +1,7 @@
+var pais = "EUA";
+console.log(`vivendo em ${pais}`)
+    if (pais == "Brasil"){
+        console.log('Você é Br')
+    }else{
+        console.log('você é Extrangeiro')
+    }
---
+++ Aula11/executando.js
@@ -0,0 +1,7 @@
+var vel = 59.5;
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+console.log('Dirija sempre usando o cinto de segurança')
+
+if(vel > 60){
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}

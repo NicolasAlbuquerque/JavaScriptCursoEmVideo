b65f7eb01ac9c95a734957f3fe31aabdf860b8a7
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-05-09 19:58:30 -0300

    Exercicido 2 da aula 12

---
+++ Aula12Ex02/script.js
@@ -0,0 +1,62 @@
+function verificar(){
+    var data =new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector("div#res")
+
+    if(fano.value.length == 0 || Number(fano.value) > ano ){
+        window.alert('[ERRO] VERIFIQUE AS INFORMAÇÕES INSERIDAS E TENTE NOVAMENTE')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade= ano - Number(fano.value)
+        res.innerHTML= `Idade Calculada ${idade}`
+        var genero=''
+        var img = document.createElement('img')//gerando uma imagem com JS
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = "Homem"
+
+            if(idade>=0 && idade <10){
+                //criança
+                img.setAttribute('src', './assets/menino.png')
+            }else if(idade >=10 && idade <= 21){
+                //jovem
+                img.setAttribute('src', './assets/jovem.png')
+            }else if (idade < 50){
+                //homem
+                img.setAttribute('src','./assets/homem.png')
+            }else {
+                //idoso
+                img.setAttribute('src', './assets/snehor.png')
+            }
+
+
+        }else if(fsex[1].checked){
+            genero ='Mulher'
+
+            if(idade>=0 && idade <10){
+                //criança
+                img.setAttribute('src', './assets/menina.png')
+            }else if(idade >=10 && idade <= 21){
+                //jovem
+                img.setAttribute('src', './assets/jova.png')
+
+            }else if (idade < 50){
+                //mulher
+                img.setAttribute('src', './assets/mulher.png')
+
+            }else {
+                //idosa
+                img.setAttribute('src', './assets/senhora.png')
+
+            }
+
+        }
+        res.style.textAlign='center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+        img.style.marginTop="3%"
+
+    }
+
+}

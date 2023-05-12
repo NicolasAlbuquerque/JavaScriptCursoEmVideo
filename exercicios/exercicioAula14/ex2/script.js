43b5c72cea4d8ac0186b339098cbe175ca7ee502
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-05-12 07:59:41 -0300

    Finalização do Exercício 2 da aula 14

--- exercicios/exercicioAula14/ex2/script.js
+++ exercicios/exercicioAula14/ex2/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function tabuada (){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if(num.value.length ==  0){
+    window.alert('Por favor, digite um número!')
+    }else{
+        var n = Number(num.value)
+        var c = 1     
+
+        tab.innerHTML=''  // limpa a info no bloco 
+        while ( c <= 10){
+        var item = document.createElement('option')//criando um elemento HTML
+            item.text = `${c} X ${c} = ${n*c}`
+            tab.appendChild(item)//adicionando um elemento filho 
+            item.value=`tab ${c}`//mostra p valor selecionado na tab
+            c++
+        }
+    }
+
+}
+
